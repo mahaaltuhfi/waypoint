758e88bb35ed0a0a007714d10890d9cd85c00d37
Maha <your-github-email@example.com>
2026-08-16 23:50:19 -0400

    Week 10: contact form with validation

--- waypoint_site/views.py
+++ waypoint_site/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from waypoint_site.forms import ContactForm
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def contact(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return render(request, "thanks.html", {"name": form.cleaned_data["name"]})
+    else:
+        form = ContactForm()
+    return render(request, "contact.html", {"form": form})
